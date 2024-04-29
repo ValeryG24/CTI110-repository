b1f5a071063a099bda80d6c40f69247d203630f1
VGonzalezftcc <166355361+ValeryG24@users.noreply.github.com>
2024-04-29 00:06:21 -0400

    Delete P1LAB1_ZyBook basics_GonzalezValery.py

--- P1LAB1_ZyBook basics_GonzalezValery.py
+++
@@ -1,7 +0,0 @@
- # Valery Gonzalez 
- # April 7, 2024
- # P1LAB1: zyLab training: Basics
- # This task requires obtaining an integer from the user through input and then outputting the square of that integer, followed by a newline character. It's worth noting that this assignment is set up for students to directly program within the zyBook platform.
-user_num = int(input())
-user_num_squared = user_num * user_num
-print(user_num_squared) # Output the squared number
