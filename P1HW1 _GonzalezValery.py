869fb2eacb6df8c946d1712391d312f98e18a96c
VGonzalezftcc <166355361+ValeryG24@users.noreply.github.com>
2024-05-03 09:05:25 -0400

    Add files via upload
    Basic output with variables.

---
+++ P1HW1 _GonzalezValery.py
@@ -0,0 +1,17 @@
+ # Valery Gonzalez
+ # April 7, 2024
+ # P1HW1 LAB: Warm up: Basic output with variables
+ # warm-up exercise aimed at preparing students for more comprehensive programming assignments. It involves using variables to store values, such as integers, and extending a given program according to specific instructions. 
+
+num1 = int(input("Enter integer:"))
+print()
+print("You entered:", num1)
+print(f"{num1} squared is {num1 ** 2}")
+print(f"And {num1} cubed is {num1 ** 3} !!")
+num2 = int(input("Enter another integer:"))
+print()
+print(f"{num1} + {num2} is {num1 + num2}")
+print(f"{num1} * {num2} is {num1 * num2}")
+
+
+
