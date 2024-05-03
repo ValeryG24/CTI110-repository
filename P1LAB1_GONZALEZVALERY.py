fa6f1182749a3588e9c071f67e6f17de552216fc
VGonzalezftcc <166355361+ValeryG24@users.noreply.github.com>
2024-05-03 08:57:38 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_GONZALEZVALERY.py
@@ -0,0 +1,11 @@
+# Valery Gonzalez
+# April 7, 2024
+# This program prompts the user to enter their first name and last name separately, then displays a welcome message to the user.
+#P1LAB1
+
+# Prompt the user to enter their first name
+first_name = input("Enter your first name: ")
+# Prompt the user to enter their last name
+last_name = input("Enter your last name: ")
+#Display a welcome message to the user
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110.")
