d97b70057191a6f9fe703872fc104b780b05db7b
VGonzalezftcc <166355361+ValeryG24@users.noreply.github.com>
2024-05-04 01:39:20 -0400

    Add files via upload
    Dictionary

---
+++ P2LAB2_GonzalezValery.py
@@ -0,0 +1,30 @@
+#Valery Gonzalez
+#April, 12, 2024
+#P2LAB2
+#Using dictionaries
+
+cars = {'Camaro' :18.21, 'Prius' :52.36, 'Model S' :110, 'Silverado' :26}
+
+#Get the keys from dictionary
+cars_keys = cars.keys()
+
+print (cars_keys)
+
+print(*cars_keys, sep = ", ")
+
+#Get a car name from user
+car_name = input("Enter a car: ")
+
+#Get mpg for the given car
+car_mpg = cars[car_name]
+print(f"The {car_name} get {car_mpg} miles per gallon.")
+
+#Get miles from user
+miles_driven = float(input(f"How many miles will you drive the {car_name}?"))
+
+#Calculate
+gallons_needed = miles_driven/car_mpg
+
+#Display results
+print(f"{gallons_needed: .2f} gallon(s) of gas are needed to drive {car_name} {miles_driven} miles")
+                     
